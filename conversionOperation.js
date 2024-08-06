6bab4f668768d0039a3c224a76b5ee61600ebd64
SaDivyasha <159869259+SaDivyasha@users.noreply.github.com>
2024-08-06 07:09:14 +0000

    conversionofDatatypes

---
+++ conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+// "33abc" => NAN (not a number) but type is number
+// true => 1; false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//""=> false
+//"hitesh" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
