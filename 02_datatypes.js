c040e8dc9189daba6785229aca1338fc6ba859cf
SaDivyasha <159869259+SaDivyasha@users.noreply.github.com>
2024-08-06 06:55:31 +0000

    dtataypes

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -4,3 +4,14 @@
 
 console.log(3+3);
 console.log("Divyanshi")
+
+//number=>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof age);
