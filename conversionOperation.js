af986b52e99bb3df869038bd66b00b69be633943
SaDivyasha <159869259+SaDivyasha@users.noreply.github.com>
2024-08-07 05:13:45 +0000

    conversions

--- conversionOperation.js
+++ conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //"33" => 33
 // "33abc" => NAN (not a number) but type is number
@@ -14,12 +14,65 @@ console.log(valueInNumber);
 let isLoggedIn = 0
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true; 0 => false
 //""=> false
 //"hitesh" => true
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//*******************Operations******** */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1 +str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 +2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+//console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+//links to study (documentation)
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+//https://262.ecma-international.org/
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined ==0);
+// console.log(undefined >0);
+// console.log(undefined <0);
+
+ //=== => checks datatypes
+
+// console.log("2" === 2);
+
