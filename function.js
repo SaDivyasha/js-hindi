2ae61c84eb6b1c9f8e119c935cc23c52b0799407
SaDivyasha <159869259+SaDivyasha@users.noreply.github.com>
2024-08-10 06:44:17 +0000

    Functions

--- function.js
+++ function.js
@@ -32,5 +32,33 @@ function loginUserMessage(username){
     return '${username} just logged in'
 }
 
-console.log(loginUserMessage("divyanshi"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage("divyanshi"));
+// console.log(loginUserMessage());
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,400,500,600));
+
+const user = {
+    username: "Divyanshi",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log('Username is ${anyobject.username} and price is ${anyobject.price}');
+
+}
+
+handleObject(user)
+
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondaryValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondaryValue(myNewArray));
