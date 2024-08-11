a63fe0a6a73afe4451eed1b13cde4037fb2b8a8e
SaDivyasha <159869259+SaDivyasha@users.noreply.github.com>
2024-08-11 13:46:56 +0000

    switch

---
+++ switch.js
@@ -0,0 +1,20 @@
+const month = 3
+
+switch (month) {
+    case1:
+        console.log("January");
+        break;
+    }
+    case2:
+        console.log("Feb");
+        break;
+    case3:
+        console.log("March");
+        break;
+    case4:
+        console.log("April");
+        break;
+    default:
+        console.log("Wrong input");
+        break;
+
