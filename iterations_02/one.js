7c7b471bd553a13c3baf24a81f56e80aa9100f8e
SaDivyasha <159869259+SaDivyasha@users.noreply.github.com>
2024-08-13 15:23:47 +0000

    filter map and reduce

---
+++ iterations_02/one.js
@@ -0,0 +1,24 @@
+// const coding = ["js", "ruby", "cpp", "sql", "python"]
+
+// const values = coding.forEach((item) => {
+//     console.log(item);
+//     return item
+// } )
+
+// console.log(values);
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//  const newNums = myNums.filter( (num) => {
+//     return num>4
+//  } )
+
+// const newNums = ["1", "2", "3", "4", "5"]
+
+// newNums.forEach( (num) => {
+//     if (num>4){
+//         newNums.push(num)
+//     }
+// })
+//  console.log(newNums);
+
